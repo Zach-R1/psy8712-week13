b679c3eab2c8274caa591febef42754848663671
Zachariah R. Roper <roper063@umn.edu>
2026-04-27 00:05:38 -0500

    Finish comments and analysis

--- R/week13-dplyr.R
+++ R/week13-dplyr.R
@@ -23,9 +23,33 @@ write.csv(testscores_tbl, "../data/testscores.csv", row.names = FALSE) # Save cs
 write.csv(offices_tbl, "../data/offices.csv", row.names = FALSE) # Save csv to data file
 
 week13_tbl <- employees_tbl %>%
-  inner_join(testscores_tbl, by = "employee_id") %>%
-  inner_join(offices_tbl, by = c("city" = "office"))
+  inner_join(testscores_tbl, by = "employee_id") %>% # join employees w/ test scors using their id removing employees w/o test scores
+  inner_join(offices_tbl, by = c("city" = "office")) # join offices dataset, matches city to office and adds appropriate office type 
 
-write.csv(week13_tbl, "../out/week13.csv", row.names = FALSE)
+write.csv(week13_tbl, "../out/week13.csv", row.names = FALSE) # Save as csv to out
 
+# Analysis
+week13_tbl %>%
+  filter(manager_hire == "Y") %>% # Filter down to manager hires
+  summarise(total_managers = n()) # Summarize down to one row to obtain manager count
 
+week13_tbl %>%
+  filter(manager_hire == "Y") %>% # Filter down to manager hires
+  summarise(unique_managers = n_distinct(employee_id)) # Summarize down to one row to obtain manager count
+
+week13_tbl %>%
+  filter(manager_hire == "N") %>% # Filter down to not originally manager hires
+  group_by(city) %>% # Group by the city
+  summarise(num_managers = n()) # Provide summary
+
+week13_tbl %>%
+  group_by(performance_group) %>% # This was shorter than using factor and got the same result
+  summarise(
+    mean_years = mean(yrs_employed, na.rm = TRUE), # Calculate mean years employed
+    sd_years = sd(yrs_employed, na.rm = TRUE) # Calculate sd years employed
+  )
+
+week13_tbl %>%
+  filter(manager_hire == "Y") %>% # Filter for manager hire
+  select(office_type, employee_id, test_score) %>% # Select relevant variables
+  arrange(office_type, desc(test_score)) # Arrange first alphabetically by office type then by test score in desc order
