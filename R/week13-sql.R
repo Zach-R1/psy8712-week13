1c40692dccfcf729eccf5f97049205b33fc35121
Zachariah R. Roper <roper063@umn.edu>
2026-04-27 00:44:56 -0500

    SQL data import

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -13,5 +13,15 @@ con <- dbConnect( # I'm considering connecting to the db Resources, but if thats
   sslmode = "require")
 
 # Data Import and Cleaning
+week13_tbl <- dbGetQuery(con, " 
+                         SELECT *
+                         FROM datascience_employees 
+                         INNER JOIN datascience_testscores
+                         ON datascience_employees.employee_id = datascience_testscores.employee_id
+                         INNER JOIN datascience_offices
+                         ON datascience_employees.city = datascience_offices.office;")
+# The duplicate id coloumn bugs me, but used inner joins in sql the same way i did in the other document used table.coloumn referencing
+
+
 
 
