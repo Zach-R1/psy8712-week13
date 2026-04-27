eb09e0efb91164feb26607069cec15a10393b334
Zachariah R. Roper <roper063@umn.edu>
2026-04-27 00:57:58 -0500

    Items 1-3, deleted data import section

--- R/week13-sql.R
+++ R/week13-sql.R
@@ -12,16 +12,34 @@ con <- dbConnect( # I'm considering connecting to the db Resources, but if thats
   port = 5432,
   sslmode = "require")
 
-# Data Import and Cleaning
-week13_tbl <- dbGetQuery(con, " 
-                         SELECT *
-                         FROM datascience_employees 
-                         INNER JOIN datascience_testscores
-                         ON datascience_employees.employee_id = datascience_testscores.employee_id
-                         INNER JOIN datascience_offices
-                         ON datascience_employees.city = datascience_offices.office;")
-# The duplicate id coloumn bugs me, but used inner joins in sql the same way i did in the other document used table.coloumn referencing
 
 
+
+# Analysis
+dbGetQuery(con, "
+           SELECT COUNT(*) AS total_managers
+           FROM datascience_employees
+           INNER JOIN datascience_testscores
+            ON datascience_employees.employee_id = datascience_testscores.employee_id
+           WHERE manager_hire = 'Y';
+           ")
+# The duplicate id coloumn bugs me, but used inner joins in sql the same way i did in the other document used table.coloumn referencing. So only data with test scores
+
+dbGetQuery(con, "
+           SELECT COUNT(DISTINCT datascience_employees.employee_id) AS unique_managers
+           FROM datascience_employees
+           INNER JOIN datascience_testscores
+            ON datascience_employees.employee_id = datascience_testscores.employee_id
+           WHERE manager_hire = 'Y';
+           ")
+
+dbGetQuery(con, "
+           SELECT city, COUNT(*) AS num_managers
+           FROM datascience_employees
+           INNER JOIN datascience_testscores
+            ON datascience_employees.employee_id = datascience_testscores.employee_id
+           WHERE manager_hire = 'N'
+           GROUP BY city;
+           ")
 
 
